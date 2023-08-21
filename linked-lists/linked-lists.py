2311729d4dd468daf017e251a61b0f1c62e9effe
ChloeHeisenberg <136772295+ChloeHeisenberg@users.noreply.github.com>
2023-08-21 15:34:22 -0400

    Add remove node method

--- linked-lists/linked-lists.py
+++ linked-lists/linked-lists.py
@@ -57,7 +57,55 @@ class LinkedList:
         for current_node in self:
             pass
         current_node.next = node
+
+# Insert a new node after an existing node
+
+    def add_after(self, target_node_data, new_node):
+        if self.head is None:
+            raise Exception('List is empty')
+        
+        for node in self:
+            if node.data == target_node_data:
+                new_node.next = node.next
+                node.next = new_node
+                return
+        
+        raise Exception("Node with data '%s' not found" % target_node_data)
     
+    def add_before(self, target_node_data, new_node):
+        if self.head is None:
+            raise Exception('List is empty')
+        
+        if self.head.data == target_node_data:
+            return self.add_first(new_node)
+        
+        prev_node = self.head
+        for node in self:
+            if node.data == target_node_data:
+                prev_node.next = new_node
+                new_node.next = node
+                return
+            prev_node = node
+
+        raise Exception("Node with data '%s' not found" % target_node_data)
+    
+    def remove_node(self, target_node_data):
+        if self.head is None:
+            raise Exception('List is empty')
+        
+        if self.head.data == target_node_data:
+            self.head = self.head.next
+            return
+        
+        previous_node = self.head
+        for node in self:
+            if node.data == target_node_data:
+                previous_node.next = node.next
+                return
+            previous_node = node
+
+        raise Exception("Node with data '%s' not found" % target_node_data)
+
 llist = LinkedList()
 print(llist)
 
@@ -94,3 +142,52 @@ print(llist)
 
 llist.add_last(Node('f'))
 print(llist)
+
+# Exception: List is empty
+# llist = LinkedList()
+# llist.add_after('a', Node('b'))
+
+llist = LinkedList(['a', 'b', 'c', 'd'])
+print(llist)
+
+llist.add_after('c', Node('cc'))
+print(llist)
+
+#Exception: Node with data 'f' not found
+# llist.add_after('f', Node('g'))
+
+#Exception: List is empty
+# llist = LinkedList()
+# llist.add_before('a', Node('a'))
+
+llist = LinkedList(['b', 'c'])
+print(llist)
+
+llist.add_before('b', Node('a'))
+print(llist)
+
+llist.add_before('b', Node('aa'))
+llist.add_before('c', Node('bb'))
+print(llist)
+
+# Exception: Node with data 'n' not found
+# llist.add_before('n', Node('m'))
+
+# Exception: List is empty
+# llist = LinkedList()
+# llist.remove_node('a')
+
+llist = LinkedList(['a', 'b', 'c', 'd', 'e'])
+print(llist)
+
+llist.remove_node('a')
+print(llist)
+
+llist.remove_node('e')
+print(llist)
+
+llist.remove_node('c')
+print(llist)
+
+# Exception: Node with data 'a' not found
+# llist.remove_node('a')
